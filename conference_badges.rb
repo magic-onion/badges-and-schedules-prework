e9d49b83e8ec2a97334241e6eec58c0f068a2675
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:44:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,6 @@
 # Write your code here.
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(speakers)
-  while i<0
-    welcomes = []
-    welcomes.unshift("Hello, my name is" + speakers[i] + ".")
-    i += 1 
-  end
-end
-
-def assign_rooms(speakers)
-  speakers.each {|i| puts "Hello, #{speakers[i]}! You'll be assigned to room #{i + 1}!"}
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
