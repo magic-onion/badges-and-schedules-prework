495690d92e23bdf5ecfe8b6ae75ddb1098dda9aa
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:50:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,10 @@ def batch_badge_creator(array_of_names)
   array_of_badge_messages=[]
   i = 0
   name = array_of_names[i]
-  welcome_message = "Hello, my name is #{name}."
   while i < array_of_names.length
+  
+  welcome_message = "Hello, my name is #{name}."
+  
     array_of_badge_messages.unshift(welcome_message)
     i += 1 
   end
