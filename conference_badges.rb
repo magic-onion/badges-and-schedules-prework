74113410f65253aca835b64e98df4d4b20b872d2
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:39:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.each {|i| puts "Hello, #{speaker[i]}! You'll be assigned to room #{i + 1}!"}
+  speakers.each {|i| puts "Hello, #{speakers[i]}! You'll be assigned to room #{i + 1}!"}
 end
 
