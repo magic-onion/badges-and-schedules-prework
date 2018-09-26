9a0a3909483f629dbe64ad9b7ea783f5d12078ec
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:42:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,11 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
-  
+  while i<0
+    welcomes = []
+    welcomes.unshift("Hello, my name is" + speakers[i] + ".")
+    i += 1 
+  end
 end
 
 def assign_rooms(speakers)
