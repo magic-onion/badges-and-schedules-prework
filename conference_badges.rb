712e5929c51382cedb583f303cc2d7d3a73fc2e5
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:39:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+  
+end
+
+def assign_rooms(speakers)
+  speakers.each {|i| puts "Hello, #{speaker[i]}! You'll be assigned to room #{i + 1}!"}
+end
+
